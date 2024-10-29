f86d0a3a7da38f15905a17e9fc7748992b7eb698
Daniil Zotov <danjke99@yandex.ru>
2024-10-29 18:11:35 +0300

    deleted unnecessary packages

--- src/test/java/pages/CheckPage.java
+++ src/test/java/pages/CheckPage.java
@@ -9,13 +9,7 @@ public class CheckPage {
 
     private SelenideElement
             menuHeaderLink = $(".header-info");
-//            lastNameInput = $("#lastName"),
-//            userEmailInput = $("#userEmail"),
-//            currentAddressInput = $("#currentAddress"),
-//            subjectsInput = $("#subjectsInput"),
-//            hobbiesInput = $("#hobbiesWrapper"),
-//            uploadPictureInput = $("#uploadPicture"),
-//            submitClick=$("#submit");
+
 
     public CheckPage openPage() {
         open("https://www.speechpro.ru/");
@@ -31,89 +25,6 @@ public class CheckPage {
         return this;
     }
 }
-//
-//    public CheckPage setLastName(String value) {
-//        lastNameInput.setValue(value);
-//
-//        return this;
-//    }
-//
-//    public CheckPage setUserEmail(String value) {
-//        userEmailInput.setValue(value);
-//
-//        return this;
-//    }
-//
-//    public CheckPage setGender(String value) {
-//        $("#genterWrapper").$(byText(value)).click();
-//
-//        return this;
-//    }
-//
-//    public CheckPage setNumber(String value) {
-//        $("#userNumber").setValue(value);
-//
-//        return this;
-//    }
-//
-//    public CheckPage setBirthDate(String day, String month, String year) {
-//        $("#dateOfBirthInput").click();
-//        calendarComponent.setDate(day, month, year);
-//
-//        return this;
-//    }
-//
-//    public CheckPage verifyResultsModalAppears() {
-//        registrationResultsModal.verifyModalAppears();
-//
-//        return this;
-//
-//    }
-//
-//    public CheckPage verifyResults(String key, String value) {
-//        registrationResultsModal.verifyResults(key, value);
-//
-//        return this;
-//
-//    }
-//
-//    public CheckPage setCurrentAddress(String value) {
-//        currentAddressInput.setValue(value);
-//
-//        return this;
-//    }
-//
-//    public CheckPage setSubjects(String value) {
-//        subjectsInput.setValue(value).pressEnter();
-//
-//        return this;
-//    }
-//
-//    public CheckPage setHobbies(String value) {
-//        hobbiesInput.$(byText(value)).click();
-//
-//        return this;
-//    }
-//
-//    public CheckPage uploadPicture(String value) {
-//        uploadPictureInput.uploadFromClasspath(value);
-//
-//        return this;
-//    }
-//
-//    public CheckPage setPlaceOfLocation(String state, String city) {
-//        locationComponent.setLocation(state, city);
-//
-//        return this;
-//    }
-//
-//    public CheckPage clickSubmit() {
-//        submitClick.click();
-//        return this;
-//    }
-
-
-
 
 
 
--- src/test/java/tests/SpeechPro/CheckMenuTest.java
+++ src/test/java/tests/SpeechPro/CheckMenuTest.java
@@ -2,8 +2,6 @@ package tests.SpeechPro;
 
 import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 public class CheckMenuTest extends TestBase {
@@ -19,10 +17,7 @@ public class CheckMenuTest extends TestBase {
     void checkMenuTest() {
 
         step("Проверяем на наличие верхнего меню сайта", () -> {
-//            $(".header-info").shouldHave(text("Компания"));
-//            $(".header-info").shouldHave(text("Новости"));
-//            $(".header-info").shouldHave(text("Карьера"));
-//            $(".header-info").shouldHave(text("Контакты"));
+
             checkPage.checkMenuHeader("Компания");
             checkPage.checkMenuHeader("Новости");
             checkPage.checkMenuHeader("Карьера");
